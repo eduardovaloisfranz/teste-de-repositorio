00eba4351b1cc9a0337877b124c4c538bd857fbe
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-29 16:43:12 -0300

    docs: documentacao da implementacao que diz se numero e par ou nao

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ console.log("Ola mundo")
 
 console.log("sem escândalo familia")
 
+/**
+ * @example verifica se o número é par ou nao
+ * @param {number} num numero para analizar
+ * @param {boolean} isEven variavel que indicará se este número é par ou não
+ */
 isEven = false
 let num = 4;
 if(num %2 === 0){
