d7d50417d7183be7898acc036d1bebaecaad4ff4
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-29 16:47:59 -0300

    style: remocao de linhas desncessárias

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 console.log("Ola mundo")
-
 console.log("sem escândalo familia")
-
 /**
  * @example verifica se o número é par ou nao
  * @param {number} num numero para analizar
  * @param {boolean} isEven variavel que indicará se este número é par ou não
  */
 function isEven(number){
-    if(number %2 === 0)
-    {
+    if(number %2 === 0) {
      return true
     } else {
         return false
