fd50678790fca6aca243984506222d4bd8c73d1c
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 22:02:01 -0300

    refactor: alteracao da variavel em portugues para ingles
    teraSorte para isHaveLucky se adequando ao padrão das outras demais variaveis em ingles

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ console.log("Ola mundo");
 console.log("sem escândalo familia");
 console.log(isEven(4));
 let currentYear = new Date().getFullYear();
-let teraSorte = Math.random() < 0.5
-console.log(`Hoje estamos no ano de ${currentYear} e o proximo ano será ${currentYear + 1}\nTerá sorte no próximo Ano? ${teraSorte}`)
+let isHaveLucky = Math.random() < 0.5
+console.log(`Hoje estamos no ano de ${currentYear} e o proximo ano será ${currentYear + 1}\nTerá sorte no próximo Ano? ${isHaveLucky}`)
 console.log(`Adeus - ${currentYear}`);
