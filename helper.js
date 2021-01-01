634f258aac4f7f8cc23f2a86c77318c09e729ff5
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 21:48:07 -0300

    docs: adicionado documentacao referente ao metodo isEven

--- helper.js
+++ helper.js
@@ -1,3 +1,9 @@
+/**
+ * @example
+ * Funcão que recebe um número e retorna se é par ou não
+ * @param {number} number numero a ser calculado se é par ou não
+ * @returns {boolean}
+ */
 export const isEven = (number) => {
     return number %2 === 0
 }
