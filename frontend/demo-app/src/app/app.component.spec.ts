c1b46c0240be06592574676bf0d340b72173aa50
eduardo valois franz <eduardo.franz@berkan.com.br>
2021-01-01 18:46:49 -0300

    Revert "test(frontend): remocao de testes desnecessarios"
    This reverts commit 418ac6d8c9423a719ca4b4e54109b9c17d6d9ff2.

--- frontend/demo-app/src/app/app.component.spec.ts
+++ frontend/demo-app/src/app/app.component.spec.ts
@@ -1,8 +1,31 @@
-describe('AppComponent', () => {
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-  it('True === true?', () => {
-  
-    expect(true).toBe(true);
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
   });
 
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'demo-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('demo-app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('demo-app app is running!');
+  });
 });
