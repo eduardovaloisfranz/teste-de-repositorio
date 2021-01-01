edf61285aebd2a79dfa46e679428625aade4a076
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 21:50:32 -0300

    docs: documentao corrigida referente ao tipo de retorno do metodo isEven

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@
  * @example
  * Funcão que recebe um número e retorna se é par ou não
  * @param {number} number numero a ser calculado se é par ou não
- * @returns {boolean}
+ * @returns {boolean} boolean
  */
 export const isEven = (number) => {
     return number %2 === 0
