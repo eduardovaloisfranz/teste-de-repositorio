202a4efd1a2ca7e6a0b34c9f1a5ccd81b2ffcd5e
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 21:44:29 -0300

    refactor: adicionado modulo helper que exporta funcoes reutilizaveis

---
+++ helper.js
@@ -0,0 +1,4 @@
+export const isEven = (number) => {
+    console.log(number)
+    return number %2 === 0
+}
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
+import { isEven } from "./helper.js";
+
+//import isEven from "helper"
 console.log("Ola mundo")
 console.log("sem escândalo familia")
-/**
- * @example verifica se o número é par ou nao
- * @param {number} num numero para analizar
- * @param {boolean} isEven variavel que indicará se este número é par ou não
- */
-function isEven(number){
-  return number %2 === 0
-}
+
 console.log(isEven(4))
 
 let currentYear = new Date().getFullYear();
