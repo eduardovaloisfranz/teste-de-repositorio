0c8e4554b50bf7155308247c2c976151b0626fff
eduardo valois franz <eduardo.franz@berkan.com.br>
2021-01-01 03:22:55 -0300

    test(api): testes de api
    testando o Get da API

---
+++ app.spec.js
@@ -0,0 +1,16 @@
+const request = require("supertest")
+const app = require("./server")
+
+describe("Testing my Api", () => {
+    it("should get main route and list persons stored", async () => {
+        const res = await request(app).get("/")
+        expect(res.body.length > 0).toBe(res.body.length > 0)
+        expect(res.body[0]).toHaveProperty('id')
+        expect(res.body[0]).toHaveProperty('nome')
+        expect(res.body[0]).toHaveProperty('idade')
+    })
+
+    it("should teste that === true", () => {
+        expect(true).toBe
+    })
+})
