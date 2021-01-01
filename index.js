40e6285e7472657579894aeaf5ae915b19575403
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 22:09:19 -0300

    feat: mensagem Mais amigavel ao usuario refente ao proximo ano
    output do isHaveLucky agora é uma mensagem amigavel para o usuário
    
    closes #3

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log("sem escândalo familia");
 console.log(isEven(4));
 let currentYear = new Date().getFullYear();
 let isHaveLucky = Math.random() < 0.5
-console.log(`Hoje estamos no ano de ${currentYear} e o proximo ano será ${currentYear + 1}\nTerá sorte no próximo Ano? ${isHaveLucky}`)
+console.log(`Hoje estamos no ano de ${currentYear} e o proximo ano será ${currentYear + 1}\nTerá sorte no próximo Ano? ${isHaveLucky ? "Sim, terá sorte no próximo ano :)" : "Não, infelizmente terá que esperar o próximo Ano :("}`)
 console.log(`Adeus - ${currentYear}`);
