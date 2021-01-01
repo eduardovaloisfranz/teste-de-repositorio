411948e879b7d095a2f442ef15f631abdd0cdca8
eduardo valois franz <eduardo.franz@berkan.com.br>
2021-01-01 01:57:41 -0300

    feat(api): criacao da api base
    esta vai ser a api que irá ser feita deploy automatizado com github actions

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const port = 3000
+
+let persons = []
+
+app.get('/', (req, res) => {
+  if(persons){
+    res.status(200)
+    res.send(persons)
+  }else {
+    res.status(404)
+    res.send("Não existem pessoas")
+  } 
+})
+
+app.post("/add", (req, res) => {
+  let personToAdd = req.body;
+  console.log(personToAdd)
+  if(personToAdd){
+    persons.push(personToAdd)
+    res.status(201)
+    res.send(personToAdd)
+  }else {
+    res.status(500)
+    res.send("Erro when trying to add the person")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
