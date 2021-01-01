07f448eea8e6b737b4732bda55fcfd79c791a574
eduardo valois franz <eduardo.franz@berkan.com.br>
2020-12-31 21:53:24 -0300

    style: corrigido identacao de codigo e adicao de ponto e virgula
    eu sinceramente acho ponto e virgula totalmente desnecessario mas eu sei que o coleguinha ai curte e
    pede pra colocar

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import { isEven } from "./helper.js";
-
-//import isEven from "helper"
-console.log("Ola mundo")
-console.log("sem escândalo familia")
-
-console.log(isEven(4))
-
+console.log("Ola mundo");
+console.log("sem escândalo familia");
+console.log(isEven(4));
 let currentYear = new Date().getFullYear();
-console.log(`Adeus - ${currentYear}`)
+console.log(`Adeus - ${currentYear}`);
